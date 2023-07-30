fc679ed8bec3a84f96af6f286ab7e5c7023a1487
Saaketh Bhojanam <saaki.bhojanam@gmail.com>
2023-07-29 22:06:29 -0500

    Added new files to make code organized

---
+++ recycling-centers.js
@@ -0,0 +1,975 @@
+var recyclingCenters = [
+            {
+
+name: "Austin Recycling Center",
+
+address: "125 W Anderson Ln, Austin, TX 78752",
+
+coordinates: [30.3105, -97.7127]
+
+},
+
+{
+
+name: "Bay Area Recycling",
+
+address: "201 Williams St, San Francisco, CA 94124",
+
+coordinates: [37.7291, -122.3970]
+
+},
+
+{
+
+name: "Boston Eco Recycling",
+
+address: "55 Harrison Ave, Boston, MA 02111",
+
+coordinates: [42.3588, -71.0578]
+
+},
+
+{
+
+name: "Brooklyn Waste Management",
+
+address: "472 Dekalb Ave, Brooklyn, NY 11205",
+
+coordinates: [40.6940, -73.9800]
+
+},
+
+{
+
+name: "Charlotte Recyclers",
+
+address: "831 Briar Creek Rd, Charlotte, NC 28205",
+
+coordinates: [35.2271, -80.8431]
+
+},
+
+// 95 more fictional entries
+
+{
+
+name: "Zenith Recycling",
+
+address: "71 W 23rd St, New York, NY 10010",
+
+coordinates: [40.7462, -73.9895]
+
+},
+
+{
+
+name: "Delaware Recycling Group",
+
+address: "361 Christina Pkwy, Wilmington, DE 19801",
+
+coordinates: [39.7424, -75.5453]
+
+},
+
+{
+
+name: "Eco Partners Miami",
+
+address: "8812 N Miami Ave, Miami, FL 33150",
+
+coordinates: [25.8928, -80.1703]
+
+},
+
+{
+
+name: "Green Rivers Recyclers",
+
+address: "612 W State St, Rockford, IL 61101",
+
+coordinates: [42.2715, -89.0917]
+
+},
+
+{
+
+name: "Indianapolis Recycling Services",
+
+address: "921 W Michigan St, Indianapolis, IN 46202",
+
+coordinates: [39.7686, -86.1464]
+
+},
+
+{
+
+name: "Kansas City Recycling Center",
+
+address: "1407 Emanuel Cleaver II Blvd, Kansas City, MO 64110",
+
+coordinates: [39.0515, -94.5558]
+
+},
+
+{
+
+name: "Nashville Waste Solutions",
+
+address: "318 Pecan St, Nashville, TN 37207",
+
+coordinates: [36.1731, -86.7382]
+
+},
+
+{
+
+name: "Orlando Sustainability Partners",
+
+address: "610 W South St, Orlando, FL 32801",
+
+coordinates: [28.5391, -81.3803]
+
+},
+
+{
+
+name: "Portland Recyclers",
+
+address: "522 NW 12th Ave, Portland, OR 97209",
+
+coordinates: [45.5327, -122.6841]
+
+},
+
+{
+
+name: "Seattle Recycling Services",
+
+address: "814 Virginia St, Seattle, WA 98101",
+
+coordinates: [47.6126, -122.3366]
+
+},
+
+{
+
+name: "Wichita Green Solutions",
+
+address: "711 S Market St, Wichita, KS 67213",
+
+coordinates: [37.6922, -97.3375]
+
+},
+
+{
+
+name: "Albuquerque Recycling",
+
+address: "421 Tijeras Ave NE, Albuquerque, NM 87102",
+
+coordinates: [35.0842, -106.6480]
+
+},
+
+{
+
+name: "Boise Recycling Center",
+
+address: "807 W Franklin St, Boise, ID 83702",
+
+coordinates: [43.6135, -116.2035]
+
+},
+
+{
+
+name: "Columbus Green Waste Solutions",
+
+address: "1421 Cleveland Ave, Columbus, OH 43211",
+
+coordinates: [39.9633, -83.0055]
+
+},
+
+{
+
+name: "Dallas Metroplex Recycling",
+
+address: "611 Elm St, Dallas, TX 75202",
+
+coordinates: [32.7766, -96.7968]
+
+},
+
+{
+
+name: "Jersey City Recyclers",
+
+address: "118 Ocean Ave, Jersey City, NJ 07305",
+
+coordinates: [40.7182, -74.0456]
+
+},
+
+{
+
+name: "Los Angeles Eco Partners",
+
+address: "421 S Spring St, Los Angeles, CA 90013",
+
+coordinates: [34.0484, -118.2501]
+
+},
+
+{
+
+name: "Madison Waste Solutions",
+
+address: "551 State St, Madison, WI 53703",
+
+coordinates: [43.0731, -89.3875]
+
+},
+
+{
+
+name: "New Orleans Recycling",
+
+address: "834 Decatur St, New Orleans, LA 70116",
+
+coordinates: [29.9571, -90.0626]
+
+},
+
+{
+
+name: "Phoenix Sustainability Center",
+
+address: "1120 S 3rd St, Phoenix, AZ 85004",
+
+coordinates: [33.4483, -112.0738]
+
+},
+
+{
+
+name: "Reno Recycling Services",
+
+address: "405 Gentry Way, Reno, NV 89502",
+
+coordinates: [39.5296, -119.8138]
+
+},
+
+{
+
+name: "Bakersfield Recycling",
+
+address: "4800 S Union Ave, Bakersfield, CA 93307",
+
+coordinates: [35.3494, -119.0187]
+
+},
+
+{
+
+name: "Cincinnati Recycling Center",
+
+address: "412 E 12th St, Cincinnati, OH 45202",
+
+coordinates: [39.1117, -84.5177]
+
+},
+
+{
+
+name: "Denver Green Partners",
+
+address: "789 Sherman St, Denver, CO 80203",
+
+coordinates: [39.7377, -104.9849]
+
+},
+
+{
+
+name: "Honolulu Sustainability Solutions",
+
+address: "1456 Kuulei Rd, Honolulu, HI 96817",
+
+coordinates: [21.2969, -157.8470]
+
+},
+
+{
+
+name: "Jacksonville Recyclers",
+
+address: "8875 San Jose Blvd, Jacksonville, FL 32217",
+
+coordinates: [30.331, -81.6471]
+
+},
+
+{
+
+name: "Milwaukee Recycling Services",
+
+address: "611 E Wisconsin Ave, Milwaukee, WI 53202",
+
+coordinates: [43.0389, -87.9065]
+
+},
+
+{
+
+name: "Minneapolis Eco Partners",
+
+address: "712 Ontario Ave SE, Minneapolis, MN 55414",
+
+coordinates: [44.9753, -93.2349]
+
+},
+
+{
+
+name: "Oklahoma City Recycling",
+
+address: "911 N Hudson Ave, Oklahoma City, OK 73102",
+
+coordinates: [35.4676, -97.5164]
+
+},
+
+{
+
+name: "Saint Louis Green Waste Solutions",
+
+address: "815 Olive St, St. Louis, MO 63101",
+
+coordinates: [38.6273, -90.1928]
+
+},
+
+{
+
+name: "Tampa Bay Recycling",
+
+address: "401 Channelside Dr, Tampa, FL 33602",
+
+coordinates: [27.9478, -82.4584]
+
+},
+
+{
+
+name: "Aurora Recycling",
+
+address: "1251 S Abilene St, Aurora, CO 80012",
+
+coordinates: [39.687, -104.852]
+
+},
+
+{
+
+name: "Cleveland Recycling Services",
+
+address: "811 Superior Ave E, Cleveland, OH 44114",
+
+coordinates: [41.501, -81.6935]
+
+},
+
+{
+
+name: "Detroit Eco Solutions",
+
+address: "1401 3rd St, Detroit, MI 48226",
+
+coordinates: [42.3314, -83.0458]
+
+},
+
+{
+
+name: "El Paso Recyclers",
+
+address: "701 Montana Ave, El Paso, TX 79902",
+
+coordinates: [31.7587, -106.4869]
+
+},
+
+{
+
+name: "Fort Worth Recycling Center",
+
+address: "2000 W Loop 820 S, Fort Worth, TX 76112",
+
+coordinates: [32.6753, -97.3196]
+
+},
+
+{
+
+name: "Louisville Green Partners",
+
+address: "1205 S Floyd St, Louisville, KY 40203",
+
+coordinates: [38.2183, -85.6636]
+
+},
+
+{
+
+name: "Mesa Recycling Services",
+
+address: "414 N Hibbert, Mesa, AZ 85201",
+
+coordinates: [33.422, -111.8315]
+
+},
+
+{
+
+name: "Tucson Recyclers",
+
+address: "1011 S Park Ave, Tucson, AZ 85719",
+
+coordinates: [32.2226, -110.9747]
+
+},
+
+{
+
+name: "Virginia Beach Recycling",
+
+address: "977 Laskin Rd, Virginia Beach, VA 23451",
+
+coordinates: [36.8469, -75.978]
+
+},
+
+{
+
+name: "Washington Eco Partners",
+
+address: "1835 14th St NW, Washington, DC 20009",
+
+coordinates: [38.917, -77.0371]
+
+},
+
+{
+
+name: "Arlington Recycling Center",
+
+address: "401 E Pioneer Pkwy, Arlington, TX 76010",
+
+coordinates: [32.7357, -97.1081]
+
+},
+
+{
+
+name: "Atlanta Recycling Services",
+
+address: "125 Ted Turner Dr NW, Atlanta, GA 30303",
+
+coordinates: [33.7557, -84.3950]
+
+},
+
+{
+
+name: "Charleston Green Waste Solutions",
+
+address: "911 Morrison Dr, Charleston, SC 29403",
+
+coordinates: [32.7957, -79.9307]
+
+},
+
+{
+
+name: "Colorado Springs Recyclers",
+
+address: "102 S Tejon St, Colorado Springs, CO 80903",
+
+coordinates: [38.8321, -104.8204]
+
+},
+
+{
+
+name: "Fresno Recycling Center",
+
+address: "1242 Van Ness Ave, Fresno, CA 93721",
+
+coordinates: [36.7377, -119.7724]
+
+},
+
+{
+
+name: "Kansas City Eco Partners",
+
+address: "911 Main St, Kansas City, MO 64105",
+
+coordinates: [39.0997, -94.5841]
+
+},
+
+{
+
+name: "Long Beach Recycling",
+
+address: "799 E Ocean Blvd, Long Beach, CA 90802",
+
+coordinates: [33.7669, -118.1883]
+
+},
+
+{
+
+name: "Memphis Green Waste Solutions",
+
+address: "977 S 3rd St, Memphis, TN 38106",
+
+coordinates: [35.1438, -90.0556]
+
+},
+
+{
+
+name: "Raleigh Recycling Services",
+
+address: "512 Fayetteville St, Raleigh, NC 27601",
+
+coordinates: [35.7752, -78.6441]
+
+},
+
+{
+
+name: "Tulsa Sustainability Center",
+
+address: "615 E 8th St, Tulsa, OK 74120",
+
+coordinates: [36.1534, -95.9931]
+
+},
+
+{
+
+name: "Anaheim Recycling Center",
+
+address: "1420 S Anaheim Blvd, Anaheim, CA 92805",
+
+coordinates: [33.8365, -117.9143]
+
+},
+
+{
+
+name: "Baltimore Green Solutions",
+
+address: "615 Clay St, Baltimore, MD 21201",
+
+coordinates: [39.2904, -76.6122]
+
+},
+
+{
+
+name: "Chandler Waste Management",
+
+address: "251 W Chicago St, Chandler, AZ 85225",
+
+coordinates: [33.3058, -111.8408]
+
+},
+
+{
+
+name: "Fort Wayne Recycling Services",
+
+address: "1200 S Clinton St, Fort Wayne, IN 46802",
+
+coordinates: [41.0756, -85.1358]
+
+},
+
+{
+
+name: "Glendale Eco Partners",
+
+address: "613 E Broadway, Glendale, CA 91205",
+
+coordinates: [34.1425, -118.2550]
+
+},
+
+{
+
+name: "Henderson Recyclers",
+
+address: "1990 W Horizon Ridge Pkwy, Henderson, NV 89012",
+
+coordinates: [36.0145, -115.058]
+
+},
+
+{
+
+name: "Irvine Sustainability Center",
+
+address: "8 Vantis Dr, Irvine, CA 92614",
+
+coordinates: [33.6695, -117.8223]
+
+},
+
+{
+
+name: "Laredo Green Waste Solutions",
+
+address: "612 Farias St, Laredo, TX 78040",
+
+coordinates: [27.5235, -99.4969]
+
+},
+
+{
+
+name: "Lubbock Recycling Center",
+
+address: "611 84th St, Lubbock, TX 79423",
+
+coordinates: [33.5779, -101.8554]
+
+},
+
+{
+
+name: "Scottsdale Recycling Services",
+
+address: "7120 E 1st Ave, Scottsdale, AZ 85251",
+
+coordinates: [33.4942, -111.9261]
+
+},
+
+{
+
+name: "Akron Recycling Center",
+
+address: "145 South High St, Akron, OH 44308",
+
+coordinates: [41.0814, -81.5190]
+
+},
+
+{
+
+name: "Baton Rouge Recyclers",
+
+address: "615 Florida St, Baton Rouge, LA 70801",
+
+coordinates: [30.4502, -91.1879]
+
+},
+
+{
+
+name: "Chula Vista Eco Solutions",
+
+address: "633 3rd Ave, Chula Vista, CA 91910",
+
+coordinates: [32.6404, -117.0886]
+
+},
+
+{
+
+name: "Fontana Waste Management",
+
+address: "14179 Foothill Blvd, Fontana, CA 92335",
+
+coordinates: [34.0922, -117.4350]
+
+},
+
+{
+
+name: "Garland Recycling Services",
+
+address: "611 Kingsley Dr, Garland, TX 75040",
+
+coordinates: [32.9147, -96.6290]
+
+},
+
+{
+
+name: "Hialeah Recycling Center",
+
+address: "625 E 25th St, Hialeah, FL 33013",
+
+coordinates: [25.8577, -80.2859]
+
+},
+
+{
+
+name: "Madison Green Solutions",
+
+address: "723 State St, Madison, WI 53703",
+
+coordinates: [43.0731, -89.3875]
+
+},
+
+{
+
+name: "Modesto Recyclers",
+
+address: "1420 10th St, Modesto, CA 95354",
+
+coordinates: [37.6390, -120.9969]
+
+},
+
+{
+
+name: "Orlando Recycling Services",
+
+address: "1201 N Orange Ave, Orlando, FL 32804",
+
+coordinates: [28.5383, -81.3792]
+
+},
+
+{
+
+name: "Reno Sustainability Center",
+
+address: "645 Kuenzli St, Reno, NV 89502",
+
+coordinates: [39.5296, -119.8138]
+
+},
+
+{
+
+name: "Chesapeake Recycling Center",
+
+address: "1413 S Military Hwy, Chesapeake, VA 23320",
+
+coordinates: [36.6778, -76.2819]
+
+},
+
+{
+
+name: "Frisco Eco Partners",
+
+address: "3391 Parkwood Blvd, Frisco, TX 75034",
+
+coordinates: [33.0871, -96.8241]
+
+},
+
+{
+
+name: "Grand Prairie Green Solutions",
+
+address: "211 SE 14th St, Grand Prairie, TX 75051",
+
+coordinates: [32.7459, -96.9928]
+
+},
+
+{
+
+name: "Huntington Beach Recyclers",
+
+address: "103 Main St, Huntington Beach, CA 92648",
+
+coordinates: [33.6595, -117.9989]
+
+},
+
+{
+
+name: "Irving Waste Management",
+
+address: "2555 N Belt Line Rd, Irving, TX 75062",
+
+coordinates: [32.8376, -96.9361]
+
+},
+
+{
+
+name: "Moreno Valley Sustainability Center",
+
+address: "14177 Frederick St, Moreno Valley, CA 92553",
+
+coordinates: [33.9425, -117.2298]
+
+},
+
+{
+
+name: "Oxnard Recycling Services",
+
+address: "751 South A St, Oxnard, CA 93030",
+
+coordinates: [34.2438, -119.1758]
+
+},
+
+{
+
+name: "Rancho Cucamonga Recyclers",
+
+address: "8739 Rochester Ave, Rancho Cucamonga, CA 91730",
+
+coordinates: [34.1063, -117.5946]
+
+},
+
+{
+
+name: "Santa Clarita Green Solutions",
+
+address: "26455 Rockwell Canyon Rd, Santa Clarita, CA 91355",
+
+coordinates:  [34.3917, -118.5407]
+
+},
+
+{
+
+name: "Warren Recycling Center",
+
+address: "29700 Hoover Rd, Warren, MI 48093",
+
+coordinates: [42.4946, -83.0319]
+
+},
+
+{
+
+name: "Chattanooga Recycling Center",
+
+address: "301 E 16th St, Chattanooga, TN 37408",
+
+coordinates: [35.0456, -85.2442]
+
+},
+
+{
+
+name: "Escondido Eco Partners",
+
+address: "200 W Mission Ave, Escondido, CA 92025",
+
+coordinates: [33.1245, -117.0864]
+
+},
+
+{
+
+name: "Hendersonville Recycling Services",
+
+address: "514 N Main St, Hendersonville, NC 28792",
+
+coordinates: [35.3187, -82.4602]
+
+},
+
+{
+
+name: "Sioux Falls Green Solutions",
+
+address: "411 N Weber Ave, Sioux Falls, SD 57104",
+
+coordinates: [43.5404, -96.7331]
+
+},
+
+{
+
+name: "Torrance Recyclers",
+
+address: "4411 Emerald St, Torrance, CA 90503",
+
+coordinates: [33.8358, -118.3406]
+
+},
+
+{
+name: "Greenleaf Recycling",
+address: "2641 Willow St, Oakland, CA 94612",
+coordinates: [-122.2662, 37.8036]
+},
+
+{
+
+name: "Eco Hub LA",
+address: "712 S Grand Ave, Los Angeles, CA 90017",
+coordinates: [-118.2591, 34.0434]
+},
+
+{
+name: "Recycle Right",
+address: "1120 Pike St, Seattle, WA 98101",
+coordinates: [-122.3386, 47.6088]
+},
+
+{
+name: "Renew Resources",
+address: "1015 N Clark St, Chicago, IL 60610",
+coordinates: [-87.6324, 41.8959]
+},
+
+{
+name: "Greenchester Recycling",
+address: "431 W Main St, Louisville, KY 40202",
+coordinates: [-85.7608, 38.2555]
+},
+
+{
+name: "Eco Partners Atlanta",
+address: "890 Peachtree St NE, Atlanta, GA 30308",
+coordinates: [-84.3925, 33.7761]
+},
+
+{
+name: "Austin Reuse Center",
+address: "2007 Kramer Ln, Austin, TX 78758",
+coordinates: [-97.7137, 30.3162]
+},
+
+{
+name: "Clean City Recycling",
+address: "108 Vine St, Philadelphia, PA 19106",
+coordinates: [-75.1521, 39.9533]
+},
+
+{
+name: "Recycle USA",
+address: "1330 New Hampshire Ave NW, Washington, DC 20036",
+coordinates: [-77.0262, 38.9007]
+},
+
+{
+name: "Green Sources NYC",
+address: "220 E 23rd St, New York, NY 10010",
+
+coordinates: [-73.9828, 40.7398]
+}
+        ];
